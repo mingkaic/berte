74198c2ee27b7861ae52a69a0dce0bf16c0c1764
mingkaic <mingkaichen2009@gmail.com>
2023-06-26 17:17:14 -0700

    Update tensorflow and dependencies versions

--- scripts/ec2_setup.sh
+++ scripts/ec2_setup.sh
@@ -1,3 +1,5 @@
+set -o xtrace
+
 sudo yum update
 
 # install pip
@@ -7,18 +9,12 @@ pip install --upgrade pip
 
 # install conda
 sudo yum install libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver -y
-wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
-sh Anaconda3-2020.02-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh
+sh Anaconda3-2023.03-1-Linux-x86_64.sh
 source ~/.bashrc
 
 # download cuda requirements
-conda install -c conda-forge cudatoolkit=11.0
-pip install nvidia-cudnn-cu11
-
-CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
-
-ln -s $CONDA_PREFIX/lib/libcusolver.so.10 $CONDA_PREFIX/lib/libcusolver.so.11 # workaround
+conda install -c conda-forge -y cudatoolkit=11.8.0
 
 # download deployment
 aws s3api get-object --bucket bidi-enc-rep-trnsformers-everywhere --key v1/pretraining/4_persentence_pretrain_mlm_15p/ec2_deployment.tar.gz ec2_deployment.tar.gz
@@ -28,6 +24,11 @@ tar -xf ec2_deployment.tar.gz
 cd workspace
 pip install -r requirements.txt
 pip install -r aws_requirements.txt
+
+CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
+
+ln -s $CONDA_PREFIX/lib/libcusolver.so.10 $CONDA_PREFIX/lib/libcusolver.so.11 # workaround
 
 # test tensorflow gpu setup
 python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
